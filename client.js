740e86e4c02e284a10afe003695644f9114cb03b
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-28 14:54:53 -0600

    handled wasd input for snake

--- client.js
+++ client.js
@@ -12,7 +12,7 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: DOB");
-    conn.write('Move: up');
+    // conn.write('Move: up');
   });
   
   conn.on('data', (data) => {
--- input.js
+++ input.js
@@ -1,12 +1,9 @@
+const { connect } = require("./client");
+let connection;
 
-  const handleUserInput = function(data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  };
-
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -15,6 +12,25 @@ const setupInput = function() {
   });
   return stdin;
 };
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+  if (data === 'w') {
+    connection.write('Move: up');
+  }
+  if (data === 'a') {
+    connection.write('Move: left');
+  }
+  if (data === 's') {
+    connection.write('Move: down');
+  }
+  if (data === 'd') {
+    connection.write('Move: right');
+  }
+};
+
 
 module.exports = {
   setupInput,
